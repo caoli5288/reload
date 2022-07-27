5b0eb060e1e2fbe0d611b57d8c88f7df01a513de
caoli5288 <caoli5288@gmail.com>
2022-07-27 17:15:25 +0800

    CommandsAltChecker.java

--- src/main/java/com/mengcraft/reload/CommandsAltChecker.java
+++ src/main/java/com/mengcraft/reload/CommandsAltChecker.java
@@ -24,12 +24,13 @@ public class CommandsAltChecker implements Listener {
         if (p.isOp()) {
             return;
         }
-        String cmd = event.getMessage().split(" ", 2)[0].substring(1);
-        Command command = Utils.getCommandMap().getCommand(cmd);
+        String cmdName = event.getMessage().split(" ", 2)[0].substring(1);
+        Command command = Utils.getCommandMap().getCommand(cmdName);
         if (command == null) {
+            event.setCancelled(true);// silently
             return;
         }
-        String cmdName = command.getName();
+        cmdName = command.getName();
         if (commands.contains(cmdName) && !p.hasPermission("command." + cmdName + ".use")) {
             event.setCancelled(true);
         }
