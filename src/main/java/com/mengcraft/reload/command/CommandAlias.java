e7c4889ddcc5974cbca5f6a168ffa5b79ebbc627
caoli5288 <caoli5288@gmail.com>
2023-04-24 15:13:51 +0800

    Improve CommandAlias.java

--- src/main/java/com/mengcraft/reload/command/CommandAlias.java
+++ src/main/java/com/mengcraft/reload/command/CommandAlias.java
@@ -4,6 +4,8 @@ import com.mengcraft.reload.PluginHelper;
 import com.mengcraft.reload.Utils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
 
 import java.util.List;
 
@@ -16,7 +18,12 @@ public class CommandAlias implements PluginHelper.IExec {
         if (command == null) {
             sender.sendMessage("§cCommand §e" + s + " §cnot found.");
         } else {
-            sender.sendMessage("§aCommand §e" + s + " §ais alias of §e" + command.getName());
+            String line = "§aCommand §e" + s + " §ais alias of §e" + command.getName();
+            if (command instanceof PluginCommand) {
+                Plugin plugin = ((PluginCommand) command).getPlugin();
+                line += " by " + plugin.getName();
+            }
+            sender.sendMessage(line);
         }
     }
 }
