5cf295895281d8c4a671e2503d0cbf2c3152bb9f
caoli5288 <caoli5288@gmail.com>
2025-01-09 11:51:27 +0800

    feat(command): add support for numerical selectors in /cmd
    - Implement functionality for selecting multiple players using numerical values

--- src/main/java/com/mengcraft/reload/command/CommandCmd.java
+++ src/main/java/com/mengcraft/reload/command/CommandCmd.java
@@ -1,12 +1,15 @@
 package com.mengcraft.reload.command;
 
+import com.google.common.collect.Lists;
 import com.mengcraft.reload.PluginHelper;
 import com.mengcraft.reload.Utils;
 import org.apache.commons.lang.math.RandomUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.NumberConversions;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -20,16 +23,27 @@ public class CommandCmd implements PluginHelper.IExec {
         if (!onlinePlayers.isEmpty() && list.size() > 1) {
             String seg = list.get(0);
             Collection<? extends Player> all = null;
-            if (seg.equals("@a")) {
-                all = onlinePlayers;
-            } else if (seg.equals("@r")) {
-                Collection<? extends Player> coll = Bukkit.getOnlinePlayers();
-                Player let = coll.stream()
-                        .skip(RandomUtils.nextInt(coll.size()))
-                        .findFirst()
-                        .orElse(null);
-                if (let != null) {
-                    all = Collections.singleton(let);
+            if (seg.charAt(0) == '@') {
+                if (seg.equals("@a")) {
+                    all = onlinePlayers;
+                } else if (seg.equals("@r")) {
+                    Collection<? extends Player> coll = Bukkit.getOnlinePlayers();
+                    Player let = coll.stream()
+                            .skip(RandomUtils.nextInt(coll.size()))
+                            .findFirst()
+                            .orElse(null);
+                    if (let != null) {
+                        all = Collections.singleton(let);
+                    }
+                } else {
+                    int pMax = NumberConversions.toInt(seg.substring(1));
+                    if (pMax >= onlinePlayers.size()) {
+                        all = onlinePlayers;
+                    } else {
+                        ArrayList<? extends Player> shuffling = Lists.newArrayList(onlinePlayers);
+                        Collections.shuffle(shuffling);
+                        all = shuffling.subList(0, pMax);
+                    }
                 }
             } else {
                 Player exact = Bukkit.getPlayerExact(seg);
